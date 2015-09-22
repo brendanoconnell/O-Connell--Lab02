2a986265ae9db04fa76df8d93d53f8bf8aabb63b
unknown <4070190@HS222-20.vtsd.com>
2015-09-22 11:53:14 -0400

    My First File

---
+++ Lab2Cv100.java
@@ -0,0 +1,24 @@
+// Lab02Cv80.java
+// This 80-point version of Lab 02C is an exercise in precisely copying a
+// provided program, followed by compilling the source code into bytecode
+// and executing the byte file
+
+public class Lab2Cv100
+{
+    public static void main(String args[]){
+        System.out.println("Lab 02C, 100 point");
+        System.out.println();
+        System.out.println(" 'First' Java Program");
+        System.out.println();
+        System.out.println("Measure today's knowledge by yesterday's confusion");
+        System.out.println();
+        System.out.println("Bewilderment + Exposure = Obvious");
+        System.out.println();
+        System.out.println("##### ##### #####   #     ###### ##### ##### ##### ");
+        System.out.println("#     #   # #   #   #     #    # #   # #   # #     ");
+        System.out.println("####  #   # #####   #     #    # #   # ##### ##### ");
+        System.out.println("#     #   # # #     #     #    # #   # #         # ");
+        System.out.println("#     ##### #   #   ##### ###### ##### #     ##### ");
+    }
+}
+        
